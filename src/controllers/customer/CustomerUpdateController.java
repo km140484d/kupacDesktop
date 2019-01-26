4333ed8f02dabd528bfc557c1c210d219ce4e13d
km183142m <mkonjikovac12@gmail.com>
2019-01-26 13:45:46 +0100

    Basic UI done.

--- src/controllers/customer/CustomerUpdateController.java
+++ src/controllers/customer/CustomerUpdateController.java
@@ -2,8 +2,14 @@ package controllers.customer;
 
 import controllers.Controller;
 import controllers.menu.CustomerMenuController;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,9 +18,98 @@ public class CustomerUpdateController extends Controller implements Initializabl
 
     @FXML
     private CustomerMenuController customerMenuController;
+    @FXML
+    private HBox editCommandBox;
+    @FXML
+    private Button editButon;
+
+    //basic info
+    @FXML
+    private TextField editName;
+    @FXML
+    private TextField editSurname;
+    @FXML
+    private TextField editPhone;
+    @FXML
+    private TextField editEmail;
+
+    //credit card
+    @FXML
+    private CheckBox editPayCheck;
+    @FXML
+    private TextField editCardNumber;
+    @FXML
+    private TextField editCardValid;
+    @FXML
+    private TextField editCardCode;
+
+    //login information
+    @FXML
+    private TextField editUsername;
+    @FXML
+    private TextField editPassword;
+    @FXML
+    private TextField editPasswordRep;
+
+    //comment
+    @FXML
+    private TextArea editComment;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         customerMenuController.emphasizeMenuItemSelected(customerMenuController.getCustMenuUpdate());
+        editCommandBox.managedProperty().bind(editCommandBox.visibleProperty());
+        editButon.managedProperty().bind(editButon.visibleProperty());
+        disableInfo(true);
+        disableCreditCardInfo(true);
+    }
+
+    public void disableInfo(boolean disable){
+        editName.setDisable(disable);
+        editSurname.setDisable(disable);
+        editPhone.setDisable(disable);
+        editEmail.setDisable(disable);
+
+        editPayCheck.setDisable(disable);
+        if (!disable && editPayCheck.isSelected())
+            disableCreditCardInfo(false);
+
+        editUsername.setDisable(disable);
+        editPassword.setDisable(disable);
+        editPasswordRep.setDisable(disable);
+
+        editComment.setDisable(disable);
+    }
+
+    public void disableCreditCardInfo(boolean disable){
+        editCardNumber.setDisable(disable);
+        editCardValid.setDisable(disable);
+        editCardCode.setDisable(disable);
+    }
+
+    public void setEditMode(boolean edit){
+        editButon.setVisible(edit);
+        editCommandBox.setVisible(!edit);
+    }
+
+    public void activateEditMode(ActionEvent actionEvent) {
+        disableInfo(false);
+        setEditMode(false);
+    }
+
+    public void saveChanges(ActionEvent actionEvent) {
+        //save changes
+        disableInfo(true);
+        setEditMode(true);
+    }
+
+    public void discardChanges(ActionEvent actionEvent) {
+        //discard changes
+        disableInfo(true);
+        setEditMode(true);
+    }
+
+    public void creditCardAction(ActionEvent actionEvent) {
+        disableCreditCardInfo(!editPayCheck.isSelected());
     }
 }
--- src/controllers/guest/RegisterController.java
+++ src/controllers/guest/RegisterController.java
@@ -2,8 +2,12 @@ package controllers.guest;
 
 import controllers.Controller;
 import controllers.menu.MainMenuController;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,8 +17,50 @@ public class RegisterController extends Controller implements Initializable {
     @FXML
     private MainMenuController menuController;
 
+    //basic info
+    @FXML
+    private TextField regName;
+    @FXML
+    private TextField regSurname;
+    @FXML
+    private TextField regPhone;
+    @FXML
+    private TextField regEmail;
+
+    //credit card
+    @FXML
+    private CheckBox regPayCheck;
+    @FXML
+    private TextField regCardNumber;
+    @FXML
+    private TextField regCardValid;
+    @FXML
+    private TextField regCardCode;
+
+    //login information
+    @FXML
+    private TextField regUsername;
+    @FXML
+    private TextField regPassword;
+    @FXML
+    private TextField regPasswordRep;
+
+    //comment
+    @FXML
+    private TextArea regComment;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         menuController.emphasizeMenuItemSelected(menuController.getRegButton());
+    }
+
+    public void disableCreditCardInfo(boolean disable){
+        regCardNumber.setDisable(disable);
+        regCardValid.setDisable(disable);
+        regCardCode.setDisable(disable);
+    }
+
+    public void creditCardAction(ActionEvent actionEvent) {
+        disableCreditCardInfo(!regPayCheck.isSelected());
     }
 }
